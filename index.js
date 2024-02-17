409e112618e4ef031cc5241004223a15d5919c49
FouzanPS <mohammedfouzanmsd7@gmail.com>
2024-02-18 00:13:28 +0530

    almost complete

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const port = 3000;
 const db = new pg.Client({
   user: "postgres",
   host: "localhost",
-  database: "world",
-  password: "fouzan@14",
+  database: "postgres",
+  password: "123456",
   port: 5432,
 });
 db.connect();
